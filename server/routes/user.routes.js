eab7c997e3ee58fa67a103a607f6e1c3077653aa
Paul Soteropulos <complexsimplicity22@yahoo.com>
2023-05-03 22:29:22 -0500

    5/3 minor route changes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,7 +5,9 @@ module.exports = (app) => {
     app.post('/api/register', UserController.registerUser),
     app.post('/api/login', UserController.loginUser)
     app.get('/api/getLoggedUser', isLoggedIn)
-    app.get('/api/logout', UserController.logOutUser)
+    app.get('/api/logout', UserController.logOutUser) //should be a POST request
     app.get('/api/view/:username', authenticate, UserController.getAllMoodsByUser)
     // app.get('/api/getLoggedUser', UserController.getLoggedUser),
 }
+// Below is what Peter said should go in every axios call with cookies (front end)
+// axios.post(url, {}, {withCredential:true})
